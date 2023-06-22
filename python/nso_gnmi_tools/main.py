9cec41bc1aa9233428ce96c59bfdd3a7a4d1cc4f
Michal Novak <micnovak@cisco.com>
2023-06-22 14:20:50 +0200

    nso-gnmi-tools - minor update, added must statement
    Signed-off-by: Michal Novak <micnovak@cisco.com>

--- python/nso_gnmi_tools/main.py
+++ python/nso_gnmi_tools/main.py
@@ -3,12 +3,12 @@ import os
 import sys
 from socket import socket, AF_INET, SOCK_STREAM
 
-import ncs
 import _ncs
 import _ncs.cdb as cdb
+import ncs
 
-from confd_gnmi_common import PORT
 from confd_gnmi_servicer import ConfDgNMIServicer, AdapterType
+
 
 # ---------------------------------------------
 # COMPONENT THREAD THAT WILL BE STARTED BY NCS.
@@ -31,35 +31,30 @@ class GnmiIter(object):
     def should_post_iterate(self, state):
         return False
 
+
 class Main(ncs.application.Application):
 
     def start_gnmi(self):
         self.log.info('=> start_gnmi')
 
-        insecure = True
         key_file = None
         crt_file = None
-        port = PORT
 
         # read configuration from CDB
         rsock = socket(AF_INET, SOCK_STREAM, 0)
-
         cdb.connect(rsock, cdb.READ_SOCKET, '127.0.0.1', _ncs.PORT)
         cdb.start_session(rsock, cdb.RUNNING)
         port = cdb.get(rsock, "/nso-gnmi-tools/port")
-        self.log.info(f"enabled cdb.get={cdb.get(rsock, '/nso-gnmi-tools/tls/enabled')}")
         insecure = cdb.get(rsock, "/nso-gnmi-tools/tls/enabled") != "true"
         if not insecure:
             if cdb.exists(rsock, "/nso-gnmi-tools/tls/keyFile"):
-                # TODO
                 key_file = str(cdb.get(rsock, "/nso-gnmi-tools/tls/keyFile"))
             if cdb.exists(rsock, "/nso-gnmi-tools/tls/certFile"):
-                # TODO
                 crt_file = str(cdb.get(rsock, "/nso-gnmi-tools/tls/certFile"))
-
-
         cdb.close(rsock)
-        self.log.info(f"insecure={insecure} key_file={key_file} crt_file={crt_file}")
+
+        self.log.info(
+            f"insecure={insecure} key_file={key_file} crt_file={crt_file}")
         self.server = ConfDgNMIServicer.serve(port, AdapterType.API,
                                               insecure=insecure,
                                               key_file=key_file,
@@ -70,7 +65,7 @@ class Main(ncs.application.Application):
     def stop_gnmi(self):
         self.log.info('=> stop_gnmi')
 
-        if hasattr(self,"server") and self.server is not None:
+        if hasattr(self, "server") and self.server is not None:
             self.server.stop(1)
             self.log.info('wait for gnmi termination')
             self.server.wait_for_termination()
@@ -80,6 +75,7 @@ class Main(ncs.application.Application):
 
     def restart_gnmi(self):
         self.log.info('=> restart_gnmi')
+
         self.stop_gnmi()
         self.start_gnmi()
 
@@ -89,6 +85,7 @@ class Main(ncs.application.Application):
         self.log.info('=> setup')
 
         sys.path.append(os.getenv('NCS_DIR') + "/src/ncs/pyapi/ncs")
+
         self.sub = ncs.cdb.Subscriber()
         self.sub.register("/nso-gnmi-tools", iter_obj=GnmiIter(
             restart_fun=self.restart_gnmi))
@@ -103,6 +100,8 @@ class Main(ncs.application.Application):
         # down, packages were reloaded or some error occurred) this teardown
         # method will be called.
         self.log.info('teardown - calling stop ')
+
         self.stop_gnmi()
         self.sub.stop()
+
         self.log.info('Main FINISHED')
